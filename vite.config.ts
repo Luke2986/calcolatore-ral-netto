317b53de0a8b26721b4f2ed464fa4042023f8c13
lucaversi <71208979+lucaversi@users.noreply.github.com>
2025-11-30 18:38:25 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-  allowedHosts: ["f10dd48c-ef03-42d6-9dde-f28f990863a5-00-1kvr83rhsxtsx.worf.replit.dev"],
+  allowedHosts: ["f3a4edaa-c465-4a29-b58c-81ac990b47b7-00-1nm18bzf7spow.worf.replit.dev"],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
